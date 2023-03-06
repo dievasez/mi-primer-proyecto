eda0f0e35cb33ab88d7bc8a1008fea664815bca4
DiegoVasquez <diegovasquez0675@gmail.com>
2023-03-06 20:54:00 -0300

    Add files via upload

---
+++ mi-primer-proyecto/js/function.js
@@ -0,0 +1,15 @@
+document.getElementById('boton').onclick = function () { 
+    console.log("primer ejemplo js");
+    document.getElementById("demo").innerHTML = "desde 05 2009 hasta 02 2022"; 
+    
+}
+
+
+document.getElementById('boton2').onclick = function () { 
+    console.log("primer ejemplo js 2");
+    document.getElementById("demo2").innerHTML = "desde 07 2004 hasta 01 2009"; 
+    
+}
+ 
+
+
